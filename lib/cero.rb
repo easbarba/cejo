54c34ca86a4f21bc9fb5a2cb281639cc8e5743da
elxbarbosa <elxbarbosa@outlook.com>
2020-09-08 16:28:17 -0300

    clean

--- lib/cero.rb
+++ lib/cero.rb
@@ -1,13 +1,2 @@
 class Cero
-  attr_accessor :venue, :name
-
-  def initialize(name, venue)
-    @venue = venue
-    @name = name
-  end
 end
-
-joe = Cero.new("a", "b")
-joe.venue = "k"
-
-puts joe.venue
