4afb38f64429cc39860cc200ad15358fb230d541
elxbarbosa <elxbarbosa@outlook.com>
2020-09-13 22:39:24 -0300

    add git cloe and pull

--- lib/services/git.rb
+++ lib/services/git.rb
@@ -1,19 +1,19 @@
+# frozen_string_literal: true
+
+# Git interface
 class Git
+  ## clone project
   def clone(url, folder)
+    system("git clone #{url} #{folder}")
   end
 
+  ## pull project
   def pull(folder)
+    Dir.chdir(folder) { system('git pull') }
   end
 
+  # archive project
   def archive(repo, to)
     puts "Archiving: #{repo} to #{to}"
-  end
-
-  def getter(repo, to)
-    if to.exist?
-      clone(repo, to)
-    else
-      pull(to)
-    end
   end
 end
