76d252b2bea69836f8315c2d5b36118f193db055
elxbarbosa <elxbarbosa@outlook.com>
2020-09-13 23:35:28 -0300

    up

--- lib/cero.rb
+++ lib/cero.rb
@@ -1,7 +1,22 @@
+# frozen_string_literal: true
 
+require_relative 'client/factories/clients_factory.rb'
+require_relative 'cli/arguments.rb'
+require_relative 'services/configure_services.rb'
+
+# Praise the sun
 class Cero
+  attr_reader :services, :clients, :arguments
 
-  def start
-    Oss.new.start("clone")
+  def initialize
+    @services = ConfigureServices.new
+    @arguments = Arguments.new.grab_guments(ARGV)
+    @clients = ClientsFactory.new(@git, @arguments)
+  end
+
+  def begin
+    @clients.sections[:operations].oss.run('get')
   end
 end
+
+Cero.new.begin
