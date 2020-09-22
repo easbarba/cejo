b72daf82c9398b3b61241e0bddf31e03be9f6b14
elxbarbosa <elxbarbosa@outlook.com>
2020-09-22 19:11:34 -0300

    project informations as collection

--- lib/cero/sections/operations/oss.rb
+++ lib/cero/sections/operations/oss.rb
@@ -8,47 +8,49 @@ module Cero
   module Operations
     # Open Source Projects utilities
     class Oss
-      attr_reader :projects, :git, :oss_projects, :home
+      attr_reader :projects, :git, :oss_projects, :data
 
       ## TODO: Use folders module
       def initialize(services)
         @git = services.git
-        @home = Dir.home
-
-        @projects = Pathname.new(File.join(home, 'Projects'))
+        @projects = Pathname.new(File.join(Dir.home, 'Projects'))
+        @data = Struct.new(:url, :name, :folder)
 
         ossfile_path = services.folders.cero_config + 'oss.json'
         @oss_projects = JSON.parse(File.read(ossfile_path))
       end
 
       def prepare(project, language, &block)
-        project_url = URI(project)
-        project_name = project_url.path.split('/').last
-        project_folder = projects.join(language, project_name)
-        puts "-- #{project_name}"
+        url = URI(project)
+        name = url.path.split('/').last
+        folder = projects.join(language, name)
 
-        block.call(project_url, project_folder, project_name, git)
+        project = data.new url, name, folder
+        block.call(project, git)
       end
 
-      ARCHIVE = lambda do |_, folder, name, git|
-        archives_folder = Pathname.new(File.join(home, 'Downloads', 'projects'))
+      ARCHIVE = lambda do |project, git|
+        archives_folder = Pathname.new(File.join(Dir.home, 'Downloads', 'projects'))
         Dir.mkdir(archives_folder) unless archives_folder.exist?
 
         archive_these = ['cero', 'lxbarbosa.github.io', 'documentos', 'ruby',
                          'rubygems', 'rubocop', 'rails', 'emacs-async',
                          'use-package', 'lsp-mode', 'emacs']
 
-        return unless archive_these.include?(name)
+        return unless archive_these.include?(project.name)
 
-        to = archives_folder + name
-        git.archive to, folder # archive thread
+        puts "-- #{project.name}"
+        to = archives_folder + project.name
+        git.archive to, project.folder # archive thread
       end
 
-      GET = lambda do |url, folder, _, git|
-        if folder.exist?
-          git.pull folder
+      GET = lambda do |project, git|
+        puts "-- #{project.name}"
+
+        if project.folder.exist?
+          git.pull project.folder
         else
-          git.clone url, folder
+          git.clone project.url, project.folder
         end
       end
 
