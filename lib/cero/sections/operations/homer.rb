8d46f473399cd0dc1e103594eda83e8ad2ebc9bd
elxbarbosa <elxbarbosa@outlook.com>
2020-09-27 12:17:01 -0300

    add Ops::Homer

--- lib/cero/sections/operations/homer.rb
+++ lib/cero/sections/operations/homer.rb
@@ -2,10 +2,40 @@
 
 module Cero
   module Ops
-    # System Volume management.
+    # Symbolic link selected folders to $HOME.
     class Homer
-      def hello
-        puts 'Volume'
+      attr_reader :home
+
+      private
+
+      def initialize
+        @home = Pathname.new(Dir.home)
+      end
+
+      def source_folders
+        Pathname.new('/dados')
+      end
+
+      def prepare_folders
+        new_folders = {}
+
+        source_folders.each_child do |folder|
+          new_folders[folder] = home.join(folder.basename)
+        end
+
+        new_folders
+      end
+
+      public
+
+      def target_link
+        prepare_folders
+      end
+
+      def run
+        target_link.each do |t, l|
+          File.symlink(t, l) if !l.exist? && t.basename.to_s != 'lost+found'
+        end
       end
     end
   end
