aa0eaf5ab2828e0d13ae17b91b969be0d4ddfa7d
elxbarbosa <elxbarbosa@outlook.com>
2020-09-28 18:23:27 -0300

    startup boot project

--- lib/cero.rb
+++ lib/cero.rb
@@ -1,40 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'cero/client/factories/clients_factory'
-require_relative 'cero/cli/arguments'
-require_relative 'cero/services/configure_services'
+require_relative 'cero/startup'
 
-# Unix Utilities.
-module Cero
-  # and so it begins...
-  class Begin
-    attr_reader :arguments, :section, :feature
-
-    private
-
-    def initialize
-      @arguments = Cero::Cli::Arguments.new.grab_arguments(ARGV)
-      @section = arguments.deq.to_sym
-      @feature = arguments.deq.to_sym
-    end
-
-    def services
-      Cero::Services::ConfigureServices.new # TODO: Use IoC
-    end
-
-    def clients
-      command = arguments.deq
-      Cero::Client::ClientsFactory.new(services, command)
-    end
-
-    public
-
-    def run
-      clients.sections[section]
-             .features[feature]
-             .run
-    end
-  end
-end
-
-Cero::Begin.new.run
+Cero::Startup.new.run
---
+++ lib/cero/startup.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative 'client/factories/clients_factory'
+require_relative 'cli/arguments'
+require_relative 'services/configure_services'
+
+# Unix Utilities.
+module Cero
+  # and so it begins...
+  class Startup
+    attr_reader :arguments, :section, :feature
+
+    private
+
+    def initialize
+      @arguments = Cero::Cli::Arguments.new.grab_arguments(ARGV)
+      @section = arguments.deq.to_sym
+      @feature = arguments.deq.to_sym
+    end
+
+    def services
+      Cero::Services::ConfigureServices.new # TODO: Use IoC
+    end
+
+    def clients
+      command = arguments.deq
+      Cero::Client::ClientsFactory.new(services, command)
+    end
+
+    public
+
+    def run
+      clients.sections[section]
+             .features[feature]
+             .run
+    end
+  end
+end
