c070c9a1c6b82286f490b6d00c290cabe129551c
eabarbosa <elxbarbosa@gmail.com>
2021-01-14 04:32:34 -0300

    rm golang version

--- cli/help.go
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Cero
-  # Provide cli interface help
-  module Cli
-    class Help
-    end
-  end
-end
--- start/version.go
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Cero
-  VERSION = '0.1.0'
-end
