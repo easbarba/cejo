32026d7e66316e9785ca7ac7dee6aba3bd796d1e
eabarbosa <elxbarbosa@gmail.com>
2021-01-19 18:35:06 -0300

    media_client/getmedia: ask for two args

--- lib/cero/client/clients/media_client.rb
+++ lib/cero/client/clients/media_client.rb
@@ -1,29 +1,36 @@
 # frozen_string_literal: true
 
 require_relative '../../sections/media/play'
+require_relative '../../sections/media/getmedia'
 
 module Cero
   module Client
     # Media features
     class MediaClient
-      attr_reader :services, :command
+      attr_reader :services, :command, :subcommand
 
       private
 
-      def initialize(services, command)
+      def initialize(services, command, subcommand)
         @services = services
         @command = command
+        @subcommand = subcommand
       end
 
       def play
         Cero::Media::Play.new(command).run
       end
 
+      def getmedia
+        Cero::Media::Getmedia.new(command, subcommand).run
+      end
+
       public
 
       def features
         {
-          play: play
+          play: play,
+          getmedia: getmedia
         }
       end
     end
