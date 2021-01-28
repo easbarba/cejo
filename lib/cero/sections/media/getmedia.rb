39c66fb90a6fdbb430ed58767c55d721a0cbf279
easbarbosa <easbarbosa@pm.me>
2021-01-28 17:38:36 -0300

    media: add getmedia

---
+++ lib/cero/sections/media/getmedia.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'pathname'
+
+require 'clipboard'
+
+module Cero
+  ## Provide system media interaction.
+  module Media
+    # Get media provided in clipboard or arguments.
+    class Getmedia
+      private
+
+      attr_reader :media, :codec
+
+      GRABBER = 'youtube-dl'
+      SUPPORTED_FORMATS = %w[vorbis flac mp3].freeze
+
+      def initialize(codec, media)
+        @media = media
+        @codec = codec
+        @media = media.nil? ? Clipboard.paste : media
+      end
+
+      def grab_audio
+        dir = Pathname.new(Dir.home).join('Music')
+        "--extract-audio --audio-format #{codec} #{media} #{dir}"
+      end
+
+      def grab_video
+        dir = Pathname.new(Dir.home).join('Videos')
+        "#{media} #{dir}"
+      end
+
+      def run_args
+        media = SUPPORTED_FORMATS.include?(@codec) ? grab_audio : grab_video
+        "#{GRABBER} #{media}"
+      end
+
+      public
+
+      def run
+        puts run_args
+        # Process.fork { system run_args }
+      end
+    end
+  end
+end
