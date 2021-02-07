eb675bb17171516930fd1649b414f1a917105180
easbarbosa <easbarbosa@pm.me>
2021-02-07 13:16:57 -0300

    ops/oss: comment methods and reorganize a bit overall class

--- lib/cejo/sections/operations/oss.rb
+++ lib/cejo/sections/operations/oss.rb
@@ -6,27 +6,20 @@ require 'uri'
 
 module Cejo
   module Ops
-    # Open Source Projects utilities
+    # Grab or Archive FLOSS Projects
     class Oss
-      ARCHIVE_THESE = ['cejo', 'lxbarbosa.github.io', 'documentos', 'ruby',
-                       'rubygems', 'rubocop', 'rails', 'emacs-async',
-                       'use-package', 'lsp-mode', 'emacs'].freeze
+      # FLOSS Projects elected to be archived
+      ARCHIVE_THESE = %w[cejo ruby rake rubocop rails use-package lsp-mode emacs].freeze
 
-      attr_reader :services, :command
+      # Skeleton information of projects: url, name, folder
+      DATA = Struct.new(:url, :name, :folder)
+
+      attr_reader :services
+      attr_reader :command
 
       private
 
-      def initialize(services, command)
-        @services = services
-        @command =  command
-
-        @git = services.git
-        @projects = Pathname.new(File.join(Dir.home, 'Projects'))
-      end
-
-      DATA = Struct.new(:url, :name, :folder)
-
-      # Archive desired FLOSS projects
+      # Archive FLOSS project
       ARCHIVE_THIS = lambda do |project, git|
         return unless ARCHIVE_THESE.include?(project.name)
 
@@ -34,50 +27,69 @@ module Cejo
         Dir.mkdir(archives_folder) unless archives_folder.exist?
 
         to = archives_folder + project.name
-        @git.archive to, project.folder # archive thread
+        git.archive to, project.folder # archive thread
       end
 
-      # Clone FLOSS Projects
-      GET_THIS = lambda do |project, git|
-        if project.folder.exist?
-          git.pull project.folder
-        else
-          git.clone project.url, project.folder
-        end
+      # Clone/Pull FLOSS Project
+      GRAB_THIS = lambda do |project, git|
+        git.pull(project.folder) if project.folder.exist?
+
+        git.clone(project.url, project.folder) unless project.folder.exist?
       end
 
-      def oss_projects
-        ossfile_path ||= services.folders.cejo_config.join('oss').join('oss.json')
-        JSON.parse(File.read(ossfile_path))
+      def initialize(services, command)
+        @services = services
+        @command =  command
       end
 
+      # Folder where Projects will be stored
+      def projects # TODO: Check if folder exist, create it otherwise
+        Pathname.new(File.join(Dir.home, 'Projects'))
+      end
+
+      # Path of file with desired FLOSS projects
+      def oss_filepath
+        services.folders.cejo_config.join('oss', 'oss.json')
+      end
+
+      # Parse FLOSS file path
+      def oss_projects_parsed
+        JSON.parse(File.read(oss_filepath))
+      end
+
+      # Provide Infomation of current FLOSS project
       def project_info(project, language)
         url = URI(project)
         name = url.path.split('/').last
-        folder = @projects.join(language, name)
+        folder = projects.join(language, name)
 
         DATA.new url, name, folder
       end
 
+      # Generate list of Projects
       def mapc(command)
-        oss_projects.each do |language, projects|
+        oss_projects_parsed.each do |language, projects|
           puts "\n--> #{language}"
-          projects.each { |project| command.call(project_info(project, language), @git) }
+          projects.each do |project|
+            command.call(project_info(project, language), @services.git)
+          end
         end
       end
 
       public
 
+      # Archive Project
       def archive
         mapc(ARCHIVE_THIS)
       end
 
-      def get
-        mapc(GET_THIS)
+      # Clone/Pull Project
+      def grab
+        mapc(GRAB_THIS)
       end
 
       def run
-        services.utils.info_and_exit(command, 'get', 'archive')
+        services.utils.info_and_exit(command, 'grab', 'archive')
 
         public_send(command)
       end
