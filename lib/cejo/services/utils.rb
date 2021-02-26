3fdc807edd3846983b5e71a5aaec0bb1787b0cd0
easbarbosa <easbarbosa@pm.me>
2021-02-26 18:15:01 -0300

    services: clean up

--- lib/cejo/services/utils.rb
+++ lib/cejo/services/utils.rb
@@ -4,55 +4,57 @@ require 'tty-spinner'
 
 require 'yaml'
 
-module Cejo::Services
-  # General Utilities.
-  class Utils
-    def command?(name)
-      [name, *ENV['PATH']
-                .split(File::PATH_SEPARATOR)
-                .map { |p| File.join(p, name) }]
-        .find { |f| File.executable?(f) }
-    end
-
-    def info_and_exit(foo, *options)
-      return unless foo.nil?
-
-      puts ''
-      puts 'Options:'
-
-      options.each { |op| puts "  option: #{op}" }
-
-      exit
-    end
-
-    def spin(msg)
-      spinner = TTY::Spinner.new("status: #{msg.downcase} :spinner ", format: :dots_6)
-
-      puts
-      spinner.auto_spin
-
-      yield
-
-      spinner.success
-      puts
-    end
-
-    # Parse Folder with serialization files
-    def parse_folder(folder)
-      projects = {}
-
-      folder.each_child do |file|
-        name = file.basename.sub_ext("").to_s.to_sym
-        projects[name] = YAML.load_file file # instead load per time to avoid errors; enumerator?
+module Cejo
+  module Services
+    # General Utilities.
+    class Utils
+      def command?(name)
+        [name, *ENV['PATH']
+                  .split(File::PATH_SEPARATOR)
+                  .map { |p| File.join(p, name) }]
+          .find { |f| File.executable?(f) }
       end
 
-      projects
-    end
+      def info_and_exit(foo, *options)
+        return unless foo.nil?
 
-    # Program is available
-    def has_program?(program)
-      ENV['PATH'].split(File::PATH_SEPARATOR).any? do |directory|
-        File.executable?(File.join(directory, program.to_s))
+        puts ''
+        puts 'Options:'
+
+        options.each { |op| puts "  option: #{op}" }
+
+        exit
+      end
+
+      def spin(msg)
+        spinner = TTY::Spinner.new("status: #{msg.downcase} :spinner ", format: :dots_6)
+
+        puts
+        spinner.auto_spin
+
+        yield
+
+        spinner.success
+        puts
+      end
+
+      # Parse Folder with serialization files
+      def parse_folder(folder)
+        projects = {}
+
+        folder.each_child do |file|
+          name = file.basename.sub_ext("").to_s.to_sym
+          projects[name] = YAML.load_file(file, symbolize_names: true)
+        end  # instead load per time to avoid errors; enumerator?
+
+        projects
+      end
+
+      # Program is available
+      def has_program?(program)
+        ENV['PATH'].split(File::PATH_SEPARATOR).any? do |directory|
+          File.executable?(File.join(directory, program.to_s))
+        end
       end
     end
   end
