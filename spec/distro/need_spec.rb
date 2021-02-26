a377086cac453784d99b98f475faeb8620c719f7
easbarbosa <easbarbosa@pm.me>
2021-02-26 19:20:45 -0300

    rspec/need: rm it word from examples

--- spec/distro/need_spec.rb
+++ spec/distro/need_spec.rb
@@ -3,17 +3,17 @@ require_relative '../../lib/cejo/sections/distro/need'
 require 'spec_helper'
 
 RSpec.describe 'The needs of one' do
-  it 'it needs to be admin' do
+  it 'needs to be admin' do
     need = Cejo::Distro::Need.new(:install)
     expect(need.admin?).to eq(true)
   end
 
-  it 'it needs arguments' do
+  it 'needs arguments' do
     need = Cejo::Distro::Need.new(:install)
     expect(need.arguments?).to eq(true)
   end
 
-  it 'it accepts whatever you wish, arguments or no' do
+  it 'accepts whatever you wish, arguments or no' do
     need = Cejo::Distro::Need.new(:autoremove)
     expect(need.whatever?).to eq(true)
   end
