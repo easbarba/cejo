7d535e92c29cdbbbb9a0ef88daadf131dfededf2
easbarbosa <easbarbosa@pm.me>
2021-02-27 01:08:39 -0300

    distro/cfg_folder: rm raw_commands

--- lib/cejo/sections/distro/config_folder.rb
+++ lib/cejo/sections/distro/config_folder.rb
@@ -6,22 +6,16 @@ module Cejo
     class ConfigFolder
       attr_reader :services
 
-      private
-
       def initialize(services)
         @services = services
       end
 
+      def folder
+        services.folders.cejo_config.join "distro"
+      end
+
       def any_config?
         folder.any?
-      end
-
-      def folder
-        services.folders.cejo_config.join 'distro'
-      end
-
-      def raw_commands
-        services.utils.parse_folder folder
       end
     end
   end
