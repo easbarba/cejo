319732b8cd50972c172774fe306b267ff1660d21
easbarbosa <easbarbosa@pm.me>
2021-02-27 00:32:26 -0300

    distro/taction: add test

--- lib/cejo/sections/distro/translate_action.rb
+++ lib/cejo/sections/distro/translate_action.rb
@@ -2,9 +2,7 @@
 
 module Cejo
   module Distro
-    # Translate Raw Action
-    class Action
-      # Translated action
+    class TranslateAction
       def real_action(commands, packager, action)
         commands[packager][action]
       end
--- spec/distro/action_spec.rb
+++ spec/distro/action_spec.rb
@@ -1,16 +1,17 @@
-require_relative '../../lib/cejo/sections/distro/action'
+require_relative "../../lib/cejo/sections/distro/translate_action"
 
-require 'spec_helper'
+require "spec_helper"
 
-RSpec.describe 'The Glorious Action' do
-  let(:action) { Cejo::Distro::Action.new }
+RSpec.describe "The Glorious Action" do
+  let(:action) { Cejo::Distro::TranslateAction.new }
 
-  it 'has the real action' do
+  it "has the real action" do
     commands = {
-      apt: { autoremove: 'autoremove' },
-      dnf: { autoremove: 'autoremove' }
+      apt: { autoremove: "autoremove" },
+      dnf: { autoremove: "autoremove" },
     }
-    real_action = action.real_action(commands , :dnf, :autoremove)
-    expect(real_action).to eq('autoremove')
+    real_action = action.real_action(commands, :dnf, :autoremove)
+
+    expect(real_action).to eq("autoremove")
   end
 end
