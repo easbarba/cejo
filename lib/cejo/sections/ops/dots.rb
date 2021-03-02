26e05b1d576ec22cab8683af1b628acebe36a8fc
easbarbosa <easbarbosa@pm.me>
2021-03-01 21:48:13 -0300

    ops/dots: refactor and clean up

--- lib/cejo/sections/ops/dots.rb
+++ lib/cejo/sections/ops/dots.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 require 'pathname'
 require 'find'
 
 module Cejo::Ops
   # Mirror Lar files in $HOME.
   class Dots
-    attr_reader :root, :ignore_these
-
-    HOME = Pathname.new(Dir.home)
+    attr_reader :root, :ignore_these, :utils, :home
 
     private
 
-    def initialize(services, root)
-      @services = services
-      @root = root
+    def initialize(utils, root)
+      @utils = utils
+      @root = Pathname.new root
+      @home = Pathname.new(Dir.home)
     end
 
     def root_files_folders
@@ -34,15 +35,17 @@ module Cejo::Ops
       { folders: folders, files: files }
     end
 
+    # transform origin file absolute path with home as its root instead
+    # /a/b/c.tar --> /home/b/c.tar
     def to_home(this)
       origin = this.to_path
-      homey = HOME.to_path.concat('/')
+      homey = home.to_path.concat('/')
       result = origin.gsub(root.to_path, homey)
-
-      Pathname.new(result)
+      result = Pathname.new(result)
     end
 
-    def create_home_folders
+    # Create only the folders, if those do not exist
+    def create_folders
       root_files_folders[:folders].each do |f|
         folder = to_home f
         next if folder.exist?
@@ -52,34 +55,42 @@ module Cejo::Ops
       end
     end
 
+    # Move file from home to a /home/backup/{file}
+    # or delete it if the file it is pointing does not exist
     def backup_this(this)
-      puts "Deleting or Moving: #{this}"
+      puts
+      print "WARNING".red, ": Deleting or moving #{this}"
+      puts
+
       this.delete if this.exist?  # TODO: if file exist back/delete up it
     end
 
-    def symlink_files # TODO: As enumerator yielding folder to symlink
+    def symlink_files
       root_files_folders[:files].each do |target|
-        next if ignore_these.include? target.basename.to_s
-
+        next if ignored_ones.include? target.basename.to_s
         symlink_name = to_home target
 
         backup_this symlink_name
 
         # TODO: Extract this to use in a block
         # TODO: Symlink only if not exist or symlkin point to nowhere
-        puts "#{target} --> #{symlink_name}"
-        symlink_name.make_symlink target
+        print "#{target}".yellow, " ❯ ", "#{symlink_name}".green
+        puts
+
+        symlink_name.make_symlink target # As enumerator yielding folder to symlink
       end
+    end
+    #
+    def ignored_ones
+      ['LICENSE', root.join('.git').to_path.to_s].freeze
     end
 
     public
 
     def run
-      @services.utils.info_and_exit(root, '/path/to/folder')
-      @root = Pathname.new(root) # TODO: check if is a folder
-      @ignore_these = ['LICENSE', root.join('.git').to_path.to_s].freeze
+      utils.info_and_exit(root, '/path/to/folder')
 
-      create_home_folders
+      create_folders
       symlink_files
     end
   end
