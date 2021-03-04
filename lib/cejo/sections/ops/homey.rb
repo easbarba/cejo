4ff82d79544f501aa46b0cfecf6463567933f398
easbarbosa <easbarbosa@pm.me>
2021-03-04 07:22:58 -0300

    ops/homey: more information to user

--- lib/cejo/sections/ops/homey.rb
+++ lib/cejo/sections/ops/homey.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 module Cejo
   module Ops
-    # Symbolic link selected folders to $HOME.
+    # Mirror user DATA partition folders to $HOME
     class Homey
       IGNORE_THESE = ['.Trash-1000', 'lost+found']
 
@@ -10,44 +12,53 @@ module Cejo
 
       def initialize(root)
         @root = prepare_root root
-        @target_link = {}
+        @target_link = find_folders
       end
 
       private
 
       # Confirms if root is a folder or return w/ exeception
       def prepare_root(root)
-        puts "root: #{root}"
+        print "root folder".blue, ": ", root
+        puts
+        puts
         Pathname.new root
       end
 
-      def prepare_folders
+      def find_folders
+        folders_found = {}
+
         home = Pathname.new(Dir.home)
         root.each_child do |folder|
-          target_link[folder] = home.join(folder.basename)
+          next if IGNORE_THESE.include? folder.basename.to_s
+          folders_found[folder] = home.join(folder.basename)
+        end
+
+        folders_found
+      end
+
+      # Remove $HOME folders found in DATA
+      def cleanup_home
+        target_link.each do |_, link_name|
+          print "Warning".red, ": ", "#{link_name}".green, " folder found, removing it!", "\n"
+          link_name.unlink if link_name.exist?
+        end
+        puts
+      end
+
+      def symlink_folders
+        target_link.each do |target, link_name|
+          print "#{target}".yellow, " --> ", "#{link_name}".green, "\n"
+          File.symlink(target, link_name)
         end
       end
 
       public
 
-      def clean_this folder
-        folder.unlink if folder.exist?
-      end
-
-      def symlink_these
-        target_link.each do |target, link_name|
-          next if IGNORE_THESE.include? target.basename.to_s
-
-          clean_this(link_name)
-
-          puts "#{target} -->  #{link_name}"
-          File.symlink(target, link_name)
-        end
-      end
-
       def run
-        prepare_folders
-        symlink_these
+        find_folders
+        cleanup_home
+        symlink_folders
       end
     end
   end
