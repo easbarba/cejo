2b0343a13298e0fc0028ed6413af327a4f008980
easbarbosa <easbarbosa@pm.me>
2021-03-04 15:38:24 -0300

    ops/volume: find pactl running sink dinamically

--- lib/cejo/sections/ops/volume.rb
+++ lib/cejo/sections/ops/volume.rb
@@ -1,115 +1,128 @@
 # frozen_string_literal: true
 
-module Cejo::Ops
-  # Manage System Volume.
-  class Volume
-    attr_reader :state, :utils
+module Cejo
+  module Ops
+    # Manage System Volume.
+    class Volume
+      attr_reader :state, :utils
 
-    STEP = 5
+      STEP = 5
 
-    def initialize(utils, state)
-      @utils = utils
-      @state = state.to_sym
-    end
+      def initialize(utils, state)
+        @utils = utils
+        @state = state.to_sym
+      end
 
-    private
+      def sound_manager
+        manager = %w[pactl amixer mixer].first { |manager| utils.which?(manager) }
+        manager.to_sym
+      end
 
-    def sound_manager
-      manager = %w[amixer pactl mixer].first { |m| utils.which?(m) }
-      manager.to_sym
-    end
+      def states
+        {
+          up: '+',
+          down: '-',
+          toggle: ''
+        }
+      end
 
-    def states
-      {
-        up: '+',
-        down: '-',
-        toggle: ''
-      }
-    end
+      # Find the running Pactl sink
+      def pactl_sink
+        sink = ''
 
-    def amixer
-      {
-        name: 'amixer',
-        toggle: '-q sset Master toggle',
-        updown: "set Master #{STEP}%#{states[state]}"
-      }
-    end
+        sinks = `pactl list sinks`
+        sinks = sinks.split "Sink #"
+        sinks.each { |a| sink = a[0] if a.include? "State: RUNNING" }
 
-    def pactl
-      {
-        name: 'pactl',
-        toggle: 'set-sink-mute 0 toggle',
-        updown: "set-sink-volume 0 #{states[state]}#{STEP}%"
-      }
-    end
+        sink
+      end
 
-    def mixer
-      {
-        name: 'mixer',
-        toggle: '',
-        updown: "mixer vol #{states[state]} #{STEP}"
-      }
-    end
+      def amixer
+        {
+          name: 'amixer',
+          toggle: '-q sset Master toggle',
+          updown: "set Master #{STEP}%#{states[state]}"
+        }
+      end
 
-    def pactl_toggle
-      "#{pactl[:name]} #{pactl[:toggle]}"
-    end
+      def pactl
+        {
+          name: 'pactl',
+          toggle: "set-sink-mute #{pactl_sink} toggle",
+          updown: "set-sink-volume #{pactl_sink} #{states[state]}#{STEP}%"
+        }
+      end
 
-    def amixer_toggle
-      "#{amixer[:name]} #{amixer[:toggle]}"
-    end
+      def mixer
+        {
+          name: 'mixer',
+          toggle: '',
+          updown: "mixer vol #{states[state]} #{STEP}"
+        }
+      end
 
-    def mixer_toggle
-      "#{mixer[:name]} #{mixer[:toggle]}"
-    end
+      def pactl_toggle
+        "#{pactl[:name]} #{pactl[:toggle]}"
+      end
 
-    def toggle
-      {
-        pactl: pactl_toggle,
-        amixer: amixer_toggle,
-        mixer: mixer_toggle
-      }
-    end
+      def amixer_toggle
+        "#{amixer[:name]} #{amixer[:toggle]}"
+      end
 
-    def pactl_updown
-      "#{pactl[:name]} #{pactl[:updown]}"
-    end
+      def mixer_toggle
+        "#{mixer[:name]} #{mixer[:toggle]}"
+      end
 
-    def amixer_updown
-      "#{amixer[:name]} #{amixer[:updown]}"
-    end
+      def toggle
+        {
+          pactl: pactl_toggle,
+          amixer: amixer_toggle,
+          mixer: mixer_toggle
+        }
+      end
 
-    def mixer_updown
-      "#{mixer[:name]} #{mixer[:updown]}"
-    end
+      def pactl_updown
+        "#{pactl[:name]} #{pactl[:updown]}"
+      end
 
-    def updown
-      {
-        pactl: pactl_updown,
-        amixer: amixer_updown,
-        mixer: mixer_updown
-      }
-    end
+      def amixer_updown
+        "#{amixer[:name]} #{amixer[:updown]}"
+      end
 
-    def mode
-      {
-        toggle: toggle[sound_manager],
-        up: updown[sound_manager],
-        down: updown[sound_manager]
-      }
-    end
+      def mixer_updown
+        "#{mixer[:name]} #{mixer[:updown]}"
+      end
 
-    public
+      def updown
+        {
+          pactl: pactl_updown,
+          amixer: amixer_updown,
+          mixer: mixer_updown
+        }
+      end
 
-    def run_args
-      mode[state]
-    end
+      def mode
+        {
+          toggle: toggle[sound_manager],
+          up: updown[sound_manager],
+          down: updown[sound_manager]
+        }
+      end
 
-    def run
-      utils.info_and_exit(state, '+', '-', 'toggle')
-      state = state.to_sym if %w[up down toggle].include? state
+      public
 
-      system(run_args)
+      def final_command
+        mode[state]
+      end
+
+      def run
+        unless [:up, :down, :toggle].include?(state)
+          utils.info_and_exit(state, 'up', 'down', 'toggle')
+          return
+        end
+
+        system(final_command)
+      end
     end
   end
 end
