6046dee9eb723970bf2b2c00b40323f49936e28f
easbarbosa <easbarbosa@pm.me>
2021-03-04 16:58:57 -0300

    ops/volume: correct *_updown syntax

--- lib/cejo/sections/ops/volume.rb
+++ lib/cejo/sections/ops/volume.rb
@@ -4,27 +4,23 @@ module Cejo
   module Ops
     # Manage System Volume.
     class Volume
-      attr_reader :state, :utils
-
       STEP = 5
+      attr_reader :state, :utils
 
       def initialize(utils, state)
         @utils = utils
         @state = state.to_sym
       end
 
-      def sound_manager
-        manager = %w[pactl amixer mixer].first { |manager| utils.which?(manager) }
-        manager.to_sym
+      def managers
+        %w[pactl amixer mixer]
       end
 
-      def states
-        {
-          up: '+',
-          down: '-',
-          toggle: ''
-        }
+      def sound_manager
+        managers.first { |manager| utils.which?(manager) }.to_sym
       end
+
+      # * PACTL
 
       # Find the running Pactl sink
       def pactl_sink
@@ -34,40 +30,66 @@ module Cejo
         end
       end
 
-      def amixer
-        {
-          name: 'amixer',
-          toggle: '-q sset Master toggle',
-          updown: "set Master #{STEP}%#{states[state]}"
-        }
+      def pactl_toggle
+        [pactl[:name], pactl[:toggle]].join(" ")
+      end
+
+      def pactl_updown
+        [pactl[:name], pactl[:updown]].join(" ")
       end
 
       def pactl
         {
           name: 'pactl',
-          toggle: "set-sink-mute #{pactl_sink} toggle",
-          updown: "set-sink-volume #{pactl_sink} #{states[state]}#{STEP}%"
+          toggle: %W[set-sink-mute #{pactl_sink} toggle].join(" "),
+          updown: %W[set-sink-volume #{pactl_sink} #{states[state]}#{STEP.to_s}%].join(" ")
         }
+      end
+
+      # * AMIXER
+
+      def amixer_toggle
+        [amixer[:name], amixer[:toggle]].join(" ")
+      end
+
+      def amixer_updown
+        [amixer[:name], amixer[:updown]].join(" ")
+      end
+
+      def amixer
+        {
+          name: 'amixer',
+          toggle: '-q sset Master toggle',
+          updown: %W[set Master #{STEP}%#{states[state]}].join("")
+        }
+      end
+
+      # * BSD MIXER
+
+      def mixer_toggle
+        [mixer[:name], mixer[:toggle]].join(" ")
+      end
+
+      def mixer_updown
+        [mixer[:name], mixer[:updown]].join(" ")
       end
 
       def mixer
         {
           name: 'mixer',
           toggle: '',
-          updown: "mixer vol #{states[state]} #{STEP}"
+          updown: %W[mixer vol #{states[state]}#{STEP}].join(" ")
         }
       end
 
-      def pactl_toggle
-        "#{pactl[:name]} #{pactl[:toggle]}"
-      end
+      # * STATES
 
-      def amixer_toggle
-        "#{amixer[:name]} #{amixer[:toggle]}"
-      end
-
-      def mixer_toggle
-        "#{mixer[:name]} #{mixer[:toggle]}"
+      def states
+        {
+          up: '+',
+          down: '-',
+          toggle: ''
+        }
       end
 
       def toggle
@@ -76,18 +98,6 @@ module Cejo
           amixer: amixer_toggle,
           mixer: mixer_toggle
         }
-      end
-
-      def pactl_updown
-        "#{pactl[:name]} #{pactl[:updown]}"
-      end
-
-      def amixer_updown
-        "#{amixer[:name]} #{amixer[:updown]}"
-      end
-
-      def mixer_updown
-        "#{mixer[:name]} #{mixer[:updown]}"
       end
 
       def updown
@@ -105,8 +115,6 @@ module Cejo
           down: updown[sound_manager]
         }
       end
-
-      public
 
       def final_command
         mode[state]
