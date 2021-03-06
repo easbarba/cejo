175c9c55eca5bf8357e5ffdc444ccbb8c934505d
easbarbosa <easbarbosa@pm.me>
2021-03-06 10:43:17 -0300

    spec/media: rename to get

--- spec/media/get_spec.rb
+++ spec/media/get_spec.rb
@@ -2,18 +2,18 @@ require_relative "../../lib/cejo"
 
 require "spec_helper"
 
-RSpec.describe "GetMedia" do
+RSpec.describe "Get" do
   it "gets me a nice tune to enjoy" do
     codec = 'vorbis'
     media = 'https://framatube.org/908ff4b-240.mp4'
-    base = Cejo::Media::Getmedia.new(media, codec)
+    base = Cejo::Media::Get.new(media, codec)
 
     expect(base.final_command).to eq("youtube-dl #{base.audio_command}")
   end
 
   it "gets me an amazing video to watch" do
     media = 'https://framatube.org/908ff4b-240.mp4'
-    base = Cejo::Media::Getmedia.new(media, nil)
+    base = Cejo::Media::Get.new(media, nil)
 
     expect(base.final_command).to eq("youtube-dl #{base.media}")
   end
