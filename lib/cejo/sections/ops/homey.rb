9f5f9b75cb912419254c281c1d4d4dea4beee3ff
easbarbosa <easbarbosa@pm.me>
2021-03-06 10:43:47 -0300

    ops/homey: disable show_info

--- lib/cejo/sections/ops/homey.rb
+++ lib/cejo/sections/ops/homey.rb
@@ -12,26 +12,25 @@ module Cejo
 
       def initialize(root)
         @root = prepare_root root
-        @target_link = find_folders
       end
 
       private
 
       # Confirms if root is a folder or return w/ exeception
       def prepare_root(root)
-        print "root folder".blue, ": ", root
-        puts
-        puts
         Pathname.new root
       end
 
       def find_folders
         folders_found = {}
-
         home = Pathname.new(Dir.home)
+
+        exit! unless root.exist?
+
         root.each_child do |folder|
           next if IGNORE_THESE.include? folder.basename.to_s
-          folders_found[folder] = home.join(folder.basename)
+
+          folders_found[folder.to_sym] = home.join(folder.basename)
         end
 
         folders_found
@@ -40,23 +39,35 @@ module Cejo
       # Remove $HOME folders found in DATA
       def cleanup_home
         target_link.each do |_, link_name|
-          print "Warning".red, ": ", "#{link_name}".green, " folder found, removing it!", "\n"
           link_name.unlink if link_name.exist?
         end
-        puts
       end
 
       def symlink_folders
         target_link.each do |target, link_name|
-          print "#{target}".yellow, " --> ", "#{link_name}".green, "\n"
           File.symlink(target, link_name)
         end
       end
 
+      # def show_info
+      #   print "root folder".blue, ": ", root, "\n\n"
+
+      #   cleanup_home do |link_name|
+      #     print "Warning".red, ": ", "#{link_name}".green, " folder found, removing it!", "\n"
+      #   end
+
+      #   puts
+
+      #   symlink_folders do |target, link_name|
+      #     print "#{target}".yellow, " --> ", "#{link_name}".green, "\n"
+      #   end
+      # end
+
       public
 
       def run
-        find_folders
+        # show_info
+        target_link = find_folders
         cleanup_home
         symlink_folders
       end
