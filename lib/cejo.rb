df91f85cf702b5474ab12a060c3d20744413c6b9
easbarbosa <easbarbosa@pm.me>
2021-04-07 05:50:07 -0300

    lib/cejo: rename to main

--- lib/cejo.rb
+++ lib/cejo.rb
@@ -17,13 +17,13 @@ require_relative 'cejo/ops/dots'
 require_relative 'cejo/ops/sysinfo'
 
 # * Floss
-require_relative 'cejo/floss/core'
+require_relative 'cejo/floss/main'
 require_relative 'cejo/floss/project_info'
 require_relative 'cejo/floss/archive'
 require_relative 'cejo/floss/grab'
 
 # * Distro
-require_relative 'cejo/distro/base'
+require_relative 'cejo/distro/main'
 require_relative 'cejo/distro/translate_action'
 require_relative 'cejo/distro/parsed_commands'
 require_relative 'cejo/distro/commands'
