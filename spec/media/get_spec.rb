e03ead6193e425a9cbdb998d861758289c7709c5
easbarbosa <easbarbosa@pm.me>
2021-04-07 06:10:31 -0300

    spec: fix distro and get

--- spec/media/get_spec.rb
+++ spec/media/get_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe 'Get' do
   let(:media) { 'https://framatube.org/908ff4b-240.mp4' }
   let(:codec) { 'vorbis' }
 
-  it 'gets me a nice tune to enjoy' do
-    base = Cejo::Media::Get.new(media, codec)
-    expect(base.final_command).to eq("youtube-dl #{base.audio_command}")
+  it 'gets me an amazing video to watch' do
+    main = Cejo::Media::Get.new(media)
+    expect(media).to eq(main.video_command)
   end
 
-  it 'gets me an amazing video to watch' do
-    base = Cejo::Media::Get.new(media)
-    expect(base.final_command).to eq("youtube-dl #{base.media}")
+  it 'gets me a nice tune to enjoy' do
+    main = Cejo::Media::Get.new(media, codec)
+    expect("--extract-audio --audio-format #{codec} #{media}").to eq(main.audio_command)
   end
 end
