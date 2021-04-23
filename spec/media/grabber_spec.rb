42442ac05ab3ca1d659430155d90224fb5e0b8b4
easbarbosa <easbarbosa@tutanota.me>
2021-04-23 00:49:22 -0300

    spec/media: clean up

--- spec/media/grabber_spec.rb
+++ spec/media/grabber_spec.rb
@@ -5,9 +5,14 @@ require_relative '../../lib/cejo'
 require 'spec_helper'
 
 RSpec.describe 'Grabber' do
+  url = 'https://odysee.com/@SystemCrafters:e/installing-the-gnu-guix-package-manager:c'
   let(:grabber) do
-    url = 'https://odysee.com/@SystemCrafters:e/installing-the-gnu-guix-package-manager:c'
     Cejo::Media::Grabber.new(url, 'mkv')
+  end
+
+  it 'audio command' do
+    grabber = Cejo::Media::Grabber.new(url, 'vorbis')
+    expect(grabber.audio_command).to eq("--extract-audio --audio-format vorbis #{url}")
   end
 
   it 'title is not too long' do
@@ -15,14 +20,9 @@ RSpec.describe 'Grabber' do
   end
 
   it 'video command' do
-    result = '--extract-audio --audio-format mkv https://odysee.com/@SystemCrafters:e/installing-the-gnu-guix-package-manager:c'
-    expect(grabber.audio_command).to eq(result)
+    expect(grabber.video_command).to eq("--recode-video mkv #{url}")
   end
 
-  it 'audio command' do
-    result = '--extract-audio --audio-format mkv https://odysee.com/@SystemCrafters:e/installing-the-gnu-guix-package-manager:c'
-    expect(grabber.audio_command).to eq(result)
-  end
   it 'sum up pretty well' do
     expect(grabber.to_s).to eq('Installing the GNU Guix Package Manager.mp4')
   end
