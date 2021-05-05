4bae3f5baeafcdfb1e9eed416cc5f8811ad87e4f
easbarbosa <easbarbosa@tutanota.me>
2021-05-05 18:14:01 -0300

    shell.nix: add commands, locale, packages; clean up

--- shell.nix
+++ shell.nix
@@ -3,13 +3,22 @@
 with pkgs;
 
 let
-  ruby = pkgs.buildPackages.ruby_2_7;
-  env = bundlerEnv {
-    name = "jamesmead.org-bundler-env";
-    inherit ruby;
-    gemdir = ./.;
-  };
+  ruby = ruby_2_7;
+  packages = [ git ruby libyaml bundler rake rubyPackages.rspec solargraph ];
+  locale = "${glibcLocales}/lib/locale/locale-archive";
+  commands = ''
+  export GEM_HOME=$PWD/.nix-gems
+  export GEM_PATH=$GEM_HOME
+  export PATH=$GEM_HOME/bin:$PATH
+  export PATH=$PWD/bin:$PATH
+
+  bundle install
+  rake install --trace
+  '';
+  shellHooks = commands;
+
 in mkShell {
-  LOCALE_ARCHIVE_2_27 = "${glibcLocales}/lib/locale/locale-archive";
-  buildInputs = [ git ruby ];
+  LOCALE_ARCHIVE_2_27 = locale;
+  buildInputs = packages;
+  shellHook = shellHooks;
 }
