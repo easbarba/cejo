b37522305c0974f7bb21e6ad8302ca92be87ccea
easbarbosa <easbarbosa@BULLSEYE-debian-live-builder>
2021-07-14 19:43:08 -0300

    main: separate roles

--- lib/cejo/main.rb
+++ lib/cejo/main.rb
@@ -6,19 +6,24 @@ require 'pathname'
 module Cejo
   # Orchestrator of project building
   class Main
-    attr_reader :builder
+    attr_reader :folders, :utils, :name
 
     def initialize(services, name)
-      folders = services.resolve :folders
-      utils = services.resolve :utils
+      @folders = services.resolve :folders
+      @utils = services.resolve :utils
+      @name = name
+    end
 
-      projects_parser = ProjectsParser.new(folders, utils, name)
-      project_info = ProjectInfo.new(folders, projects_parser.project)
+    def projects_parser
+      ProjectsParser.new(folders, utils, name)
+    end
 
-      @builder = Builder.new(utils, folders.local, project_info)
+    def project_info
+      ProjectInfo.new(folders, projects_parser.project)
     end
 
     def run
+      builder = Builder.new(utils, folders.local, project_info)
       builder.run
     end
   end
